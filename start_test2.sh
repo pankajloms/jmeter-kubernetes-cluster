f45ca62ecb55d4358f58b5ad73b4cad7ada6dc29
pankajloms <pankajloms@gmail.com>
2022-06-30 21:32:57 +0530

    Update start_test2.sh

--- start_test2.sh
+++ start_test2.sh
@@ -9,7 +9,7 @@ working_dir="`pwd`"
 tenant=`awk '{print $NF}' "$working_dir/tenant_export"`
 
 jmx="$1"
-read -p 'Enter path to the jmx file ' jmx
+read -rp 'Enter path to the jmx file ' jmx
 
 if [ ! -f "$jmx" ];
 then
